1d89b10f265c35d4096f08f0f8743f248643af85
Rimsha-Urooj <rimshaurooj56@gmail.com>
2021-09-13 12:41:14 +0500

    using useEffect Hook

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,21 +1,39 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css'
 import CartContext from '../../store/cart-context';
 
 function HeaderCartButton(props) {
 
+    const [btnIsHighlighted, setBtnIsHighLighted] = useState(false);
     const cartCtx = useContext(CartContext);
+    const {items} = cartCtx;
 
     // reduce func allow to transform an array of data into a single value
     // takes 2 arguments 1st is func, and 2nd is starting value.
     // func passed in arguments receive 2 arguments itself
-    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    const numberOfCartItems = items.reduce((curNumber, item) => {
         return curNumber + item.amount;
     }, 0);
 
+    
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump :''}`;
+    useEffect(() => {
+        if (items.length === 0){
+            return;
+        }
+        setBtnIsHighLighted(true);
+        const timer = setTimeout(() => {
+            setBtnIsHighLighted(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [items]);
+
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
